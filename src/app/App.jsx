72c209d5b7a41d05cbcd554c853fc6cae12165a1
Arman Khilko <arma.biz9805@gmail.com>
2020-12-28 22:50:13 +0400

    mend

--- src/app/App.jsx
+++ src/app/App.jsx
@@ -1,12 +1,22 @@
 // Core
 import { hot } from "react-hot-loader/root";
+import { createGlobalStyle } from "styled-components";
 
 // Styles
 import "normalize.css";
+import "_theme/main.css";
+
+const GlobalStyle = createGlobalStyle`
+    body {
+        background-color: blue;
+    }
+`;
 
 const App = () => (
     <React.StrictMode>
+        <GlobalStyle />
         <h1>Hello world</h1>
+        <span> Font check </span> 
     </React.StrictMode>
 );
 
