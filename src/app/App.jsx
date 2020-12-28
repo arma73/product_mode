4402afcf9cfa377e17c52193f8da13d4c98edc9d
Arman Khilko <arma.biz9805@gmail.com>
2020-12-28 21:31:30 +0400

    Create: initial project

---
+++ src/app/App.jsx
@@ -0,0 +1,13 @@
+// Core
+import { hot } from "react-hot-loader/root";
+
+// Styles
+import "normalize.css";
+
+const App = () => (
+    <React.StrictMode>
+        <h1>Hello world</h1>
+    </React.StrictMode>
+);
+
+export default hot(App);
